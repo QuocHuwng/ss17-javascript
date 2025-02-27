81365edcc8d02f5e8577e690b66aebeb5018dd50
QuocHuwng <nguyenhung2952006@gmail.com>
2025-02-27 20:49:20 +0700

    Add files via upload

--- baitap2.js
+++ baitap2.js
@@ -1,9 +1,4 @@
 /* 3 vi du sai quy tac */
-let 1vidu;
-let my-name;
-let let;
+document.write("Ten bien khong hop le la: let 1vidu;let my-name;let let");
 /* 3 vi du dung quy tac */
-let name;
-let _age;
-let $price;
-
+document.write("<br>Ten bien hop le la:let name; let _age; let $price");
